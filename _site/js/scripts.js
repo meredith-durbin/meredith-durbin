6b56c60722da2d13c9cdd60dfcf12ffb3ed72373
Meredith Durbin <mdurbin@stsci.edu>
2015-10-21 15:00:50 -0400

    Revision numero uno

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -1,40 +1,26 @@
-function derp() {
-  var windowHeight = $(window).height();
-  $('.section-container.transparent').each( function() {
-    var sectHeight = $(this).children("section").outerHeight();
-    var sectTop = $(this).find("section").position().top;
-    if (sectHeight < windowHeight) {
-      $(this).css('margin-top', -1.0 * sectTop - 2.);
-      $(this).css('margin-bottom', -1.0 * sectTop - 2.);
+$(document).ready(function() {
+  $('nav a[href^="#"]').click(function(e) { 
+    e.preventDefault();
+    var el = $( e.target.getAttribute('href') );
+    var hash = this.hash;
+    var navHeight = $('nav').height();
+    var elOffset = el.offset().top;
+    var elHeight = el.height();
+    var windowHeight = $(window).height();
+    var offset;
+
+    if (elHeight < windowHeight) {
+      offset = elOffset - ((windowHeight / 2) - (elHeight / 2));
     }
     else {
-      $(this).css('height', 'auto');
-      $(this).css('overflow', 'visible');
-      $(this).css('margin-bottom', 1.0 * sectTop - 2.);
-    };
+      offset = elOffset - navHeight;
+    }
+    var speed = 700;
+    $('html, body').animate({scrollTop:offset}, speed, function (){
+              location.hash = hash;
+          });
+    return false;
   });
-};
-
-derp();
-
-$(window).resize(function() {
-  derp();
-});
-
-$(window).on('hashchange', function() {
-    var hash = window.location.hash;
-    $('.navbar-nav li').removeClass("active");
-    $("a[href='" + hash + "']").parent().addClass("active");
-});
-
-$(document).ready(function () {
-    var hash = location.hash;
-    if (hash !== '') {
-        $(window).trigger('hashchange');
-    }
-    else {
-      $("a[href='#home']").parent().addClass("active");
-    }
 });
 
 // Cache selectors
@@ -50,52 +36,55 @@ var lastId,
     }),
     noScrollAction = false;
 
-// foofy scrolling
-var $root = $('html, body');
-$('a[href*=#]').click(function(e) {
-    e.preventDefault();
-    var windowHeight = $(window).height();
-    var href = $.attr(this, 'href');
-    var elHeight = $(href).outerHeight();
-    $root.animate({
-        scrollTop: $(href).offset().top + elHeight / 2. - windowHeight / 2.
-    }, 500, function () {
-        window.location.hash = href;
-    });
+$(document).ready(function () {
+    var hash = location.hash;
+    if (hash !== '') {
+        $(window).trigger('hashchange');
+    }
+    else {
+      $("a[href='#home']").parent().addClass("active");
+    }
+});
+
+$(window).on('hashchange', function() {
+    var hash = window.location.hash;
+    $('.navbar-nav li').removeClass("active");
+    $("a[href='" + hash + "']").parent().addClass("active");
 });
 
 // Highlight each nav item as you scroll past
 $(window).scroll(function(){
-   if(!noScrollAction){
-       var windowHeight = $(window).height();
-       var fromTop = $(this).scrollTop() + windowHeight / 2. + 55;
-       var cur = scrollItems.map(function(){
-         if ($(this).offset().top < fromTop - $(this).outerHeight() / 2.)
-           return this;
-       });
-       cur = cur[cur.length-1];
-       var id = cur && cur.length ? cur[0].id : "";
-       
-       if (lastId !== id) {
-           lastId = id;
-           // Set/remove active class
-           menuItems
-             .parent().removeClass("active")
-             .end().filter("[href=#"+id+"]").parent().addClass("active");
-       }
+  if(!noScrollAction){
+    var windowHeight = $(window).height();
+    var navHeight = $('nav').height();
+    var fromTop = $(this).scrollTop() + windowHeight / 1.75;
+    var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop - $(this).outerHeight() / 2.)
+       return this;
+    });
+    cur = cur[cur.length-1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+     }
    }    
 });
 
 function setToggle() {
   $("#portfolio-click").click(function() {
     if ($("#portfolio-click").text() == "Show portfolio") {
-    $("#portfolio-click").text("Hide portfolio");
-    $("#portfolio").show();
-  }
-  else {
-    $("#portfolio-click").text("Show portfolio");
-    $("#portfolio").hide();   
-  }
+      $("#portfolio-click").text("Hide portfolio");
+      $("#portfolio").show();
+    }
+    else {
+      $("#portfolio-click").text("Show portfolio");
+      $("#portfolio").hide();   
+    }
   });
 };
 
